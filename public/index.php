a671d827005bfdbadcaf44dec3616975291ca8e5
LarsDeStam <larsdestam@gmail.com>
2023-11-06 10:56:20 +0100

    test

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php require_once("../php4/includes/default.inc.php") ?>
+<!DOCTYPE html>
+<html long="en" dir="ltr">
+    <head>
+     <meta charset="utf-8">
+     <title>MVC structuur</title>
+     <link rel="stylesheet" href="css/master.css">
+    </head>
+    <body style="">
+        <?php
+
+
+            $test = new Test();
+            echo $test;
+        ?>
+    </body>
+</html>
